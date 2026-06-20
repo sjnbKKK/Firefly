8ee0d4bbed43202518efe962d68f2369b1dc97ef
fqzlr <a10275974@outlook.com>
2026-06-21 07:17:25 +0800

    Use local profile avatar image

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -6,7 +6,7 @@ export const profileConfig: ProfileConfig = {
 	// 1. public 目录（以 "/" 开头，不优化）："/assets/images/avatar.webp"
 	// 2. src 目录（不以 "/" 开头，自动优化但会增加构建时间，推荐）："assets/images/avatar.webp"
 	// 3. 远程 URL："https://example.com/avatar.jpg"
-	avatar: "https://q1.qlogo.cn/g?b=qq&nk=379446167&s=640",
+	avatar: "assets/images/avatar.avif",
 
 	// 名字
 	name: "Fqzlr",
