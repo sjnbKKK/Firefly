4c3f06c46c11814376dd8f3b4ff621fdcc9b85cb
fqzlr <a10275974@outlook.com>
2026-06-21 11:41:05 +0800

    修复赞助

--- src/config/sponsorConfig.ts
+++ src/config/sponsorConfig.ts
@@ -39,19 +39,19 @@ export const sponsorConfig: SponsorConfig = {
 			description: "使用 微信 扫码打赏",
 			enabled: true,
 		},
-		{
-			name: "ko-fi",
-			icon: "simple-icons:kofi",
-			qrCode: "",
-			link: "https://ko-fi.com/cuteleaf",
-			description: "Buy a Coffee for Firefly",
-			enabled: true,
-		},
+		// {
+		// 	name: "ko-fi",
+		// 	icon: "simple-icons:kofi",
+		// 	qrCode: "",
+		// 	link: "https://ko-fi.com/fqzlr",
+		// 	description: "Buy a Coffee for Firefly",
+		// 	enabled: true,
+		// },
 		{
 			name: "爱发电",
 			icon: "simple-icons:afdian",
 			qrCode: "",
-			link: "https://ifdian.net/a/cuteleaf",
+			link: "https://ifdian.net/a/fqzlr",
 			description: "通过 爱发电 进行打赏",
 			enabled: true,
 		},
@@ -70,9 +70,10 @@ export const sponsorConfig: SponsorConfig = {
 
 		// 示例：匿名打赏者
 		{
-			name: "匿名用户",
-			// avatar: "",
-			amount: "¥20",
+			name: "214556787",
+			avatar:
+				"https://weavatar.com/avatar/d252655d40d6874417a720bad0a6c5f77f8f6a1fd2f882f8f338402dc37e4190?s=640",
+			amount: "¥1",
 			date: "2025-10-01",
 		},
 	],
